0312a6472489a8bfe82191118772844eed96e9d1
Vladimír Fojtík <fojtik.v@gmail.com>
2019-03-03 22:15:28 +0100

    Added first implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-/* Main file */
+import * as Boom from 'Boom';
+
+export default () => {
+    return (req, res, next) => {
+        if (res.boom) throw new Error('Boom already exists on response object.');
+
+        res.boom = {};
+        const methods = Object.getOwnPropertyNames(Boom).filter((prop) => {
+            return typeof Boom[prop] == 'function';
+        });
+
+        methods.forEach((key) => {
+            res.boom[key] = function() {
+                const Boomed = Boom[key].apply(Boom, arguments);
+                const BoomedWithAdditionalResponse = Object.assign(Boomed.output.payload, arguments[1]);
+
+                return res.status(Boomed.output.statusCode).send(BoomedWithAdditionalResponse);
+            };
+        });
+
+        next();
+    };
+};
